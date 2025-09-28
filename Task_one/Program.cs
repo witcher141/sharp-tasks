e4f93d581065a691d53ed0287bf08a13071d3894
witcher141 <stratijcuku3@gmail.com>
2025-09-28 22:05:42 +0500

    Задание 1

---
+++ Task_one/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите температуру в °C: ");
+        double celsius = Convert.ToDouble(Console.ReadLine());
+
+        double fahrenheit = celsius * 9 / 5 + 32;
+
+        Console.WriteLine($"{celsius}°C = {fahrenheit}°F");
+    }
+}
