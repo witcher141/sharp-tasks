950e36a22c1e3ec9d6f7f04bf58c20ab123f8dfe
witcher141 <stratijcuku3@gmail.com>
2025-09-28 22:35:47 +0500

    Задание 3

--- Task_three/Program.cs
+++ Task_three/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Random random = new Random();
+        int secret = random.Next(1, 11);
+        
+        Console.WriteLine("Я загадал число от 1 до 10. Попробуйте угадать!");
+
+        while (true)
+        {
+            Console.Write("Ваш вариант: ");
+            int guess = Convert.ToInt32(Console.ReadLine());
+
+            if (guess == secret)
+            {
+                Console.WriteLine("Поздравляю! Вы угадали число!");
+                break;
+            }
+            else if (guess < secret)
+            {
+                Console.WriteLine("Моё число больше.");
+            }
+            else
+            {
+                Console.WriteLine("Моё число меньше.");
+            }
+        }
+    }
+}
